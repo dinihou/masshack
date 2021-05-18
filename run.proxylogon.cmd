9e0b755e93c8985ad5ff0b3587d7786d6152762b
dinihou <bennabdellah.m@gmail.com>
2021-05-18 17:51:47 +0400

    init

--- run.proxylogon.cmd
+++ run.proxylogon.cmd
@@ -2,5 +2,5 @@ set "PYTHONIOENCODING=UTF-8"
 chcp 65001
 cls
 call %cd%\venv\Scripts\activate.bat
-py ./masshack.py -ipaddr "host\\zones\\dz.zone" --scanner "exchange" --exploit "proxylogon"
+py ./masshack.py -ipaddr "host\\zones\\tn.zone" --scanner "exchange" --exploit "proxylogon"
 pause
